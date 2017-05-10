8681acb2d25540542e72e3e210d161a08c8506cb
MarkGrayson <Mpal7487@Gmail.com>
2017-05-10 13:57:18 -0600

    Created project

---
+++ src/project/controller/ProjectController.java
@@ -0,0 +1,29 @@
+package project.controller;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import project.model.*;
+
+public class ProjectController 
+{
+	private Stack<Dishes> Dishes;
+	private Queue<Customer> Customer;
+	
+	public void start()
+	{
+				
+	}
+	
+	public void Customer()
+	{
+		Customer = new LinkedList<Customer>();
+	}
+	
+	public void Dishes()
+	{
+		
+		
+	}
+}
---
+++ src/project/controller/ProjectRunner.java
@@ -0,0 +1,13 @@
+package project.controller;
+
+import project.controller.ProjectController;
+
+public class ProjectRunner 
+
+{
+	public static void main (String [] args)
+	{
+		ProjectController projectAppController = new ProjectController();
+		projectAppController.start();
+}
+}
